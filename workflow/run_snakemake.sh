2904c8011bee1997510a4131e4b8dc493ab89f2e
Jack Sumner <jacksumner@jacks-mbp.lan>
2020-11-13 11:26:04 -0600

    Changed config cluster information

--- workflow/run_snakemake.sh
+++ workflow/run_snakemake.sh
@@ -2,9 +2,7 @@
 #SBATCH -A p31288
 #SBATCH --job-name="scheduler"
 #SBATCH -t 04:00:00
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -c 1
+#SBATCH -n 10
 #SBATCH -p short
 #SBATCH --mem=2gb
 #SBATCH --mail-type=ALL
@@ -18,6 +16,3 @@ cd $SLURM_SUBMIT_DIR
 
 mkdir -p logs_slurm
 snakemake --verbose --use-conda --cluster-config cluster.yaml --cluster 'sbatch -A {cluster.allocation} -p {cluster.partition} -t {cluster.time} --mem-per-cpu={cluster.mem} -n {cluster.cpus} -o {cluster.output} -e {cluster.output} --mail-type {cluster.email_type} --mail-user {cluster.email}' -j 10
-
-source deactivate
-source deactivate
